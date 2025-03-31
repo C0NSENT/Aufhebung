31854fb3627dbb20942c978bf46bbee6c6a9ca96
consent_ <c0nsent@proton.me>
2025-03-31 22:17:31 +0300

    Обновление файлов

---
+++ C++ Primer/Test/aggregate_class.cpp
@@ -0,0 +1,18 @@
+//
+// Created by consent_ on 31-Mar-25.
+// 6a7ha3RAD7^^ki4Z4v
+
+
+#include <iostream>
+#include <string>
+
+struct Person
+{
+	int age;
+	std::string name;
+};
+
+int main()
+{
+	Person p{21};
+}
--- C++ Primer/Test/test_1.cpp
+++ C++ Primer/Test/test_1.cpp
@@ -11,27 +11,28 @@ class foo
 public:
 	 mutable int a;
 
+	explicit foo(const int& a ) : a(a) {}
+	foo(): foo(0) {}
 	foo lox() const {return *this;}
 
-	foo(const int& a ) : a(a) {}
 
 	const int& get() const { return this->a; }
 
-	int& set()  { this->a; }
+	void set(const foo& a)  { this->a = a.a;  }
 
 	int bar() const {return 1;}
+
+
 
 	void increment() const {this->a++;}
 };
 
 int main()
 {
-	foo obj{1};
-	//int& a = obj.get();
-	/*int* a;
-	a{ obj.get()};*/
+	foo obj1;
+	int a = 1;
+	obj1.set(a);
 
-	obj.increment();
 
-	std::cout << obj.a << std::endl;
+	std::cout << obj1.a << std::endl;
 }
